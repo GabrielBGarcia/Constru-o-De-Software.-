713fc279f2eceee3e37d633afcd074bf8cb147f8
GabrielBGarcia <gabrielnpro@gmail.com>
2024-04-26 13:42:43 -0300

    att8

---
+++ atividade08/src/main/java/com/ufg/Main.java
@@ -0,0 +1,11 @@
+//package com.ufg;
+//
+//import com.ufg.domain.ContaBancaria;
+//import com.ufg.domain.Login;
+//import com.ufg.exception.LoginInvalidoException;
+//
+//public class Main {
+//    public static void main(String[] args) throws LoginInvalidoException {
+//        new ContaBancaria();
+//    }
+//}
---
+++ atividade08/src/main/java/com/ufg/exception/LoginInvalidoException.java
@@ -0,0 +1,7 @@
+package com.ufg.exception;
+
+public class LoginInvalidoException extends Exception{
+    public LoginInvalidoException(String msg){
+        super(msg);
+    }
+}
